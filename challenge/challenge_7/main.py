8ccafc31ffd0ae534288eebbb2ea154e7f51a8cd
Hyojin Park <68233870+prkhyo@users.noreply.github.com>
2021-10-12 11:59:35 +0000

    challenge 7

---
+++ challenge/challenge_7/main.py
@@ -0,0 +1,31 @@
+
+
+
+# Challenge goals:
+# Using this boilerplate we are going to build a mini clone of the Hacker News Website 
+# using the Hacker News Search API and Flask.
+# 해커뉴스 API와 Flask를 활용하여 해커뉴스 웹사이트 클론코딩을 진행합니다.
+# 위의 힌트 (Clues)를 활용하여, 필요조건 (Requirements) 에 맞추어 과제를 완수하세요.
+# https://news.ycombinator.com/
+# https://hn.algolia.com/api
+# https://uniformlinednature.serranoarevalo.repl.co/
+
+
+# 클론코딩할 웹사이트는 다음과 같은 경로를 가집니다.
+# /
+# /?order_by=new
+# /?order_by=popular
+# /＜id＞
+
+
+#### 조건 #####
+# 강의 4.6와 같은 fakeDB를 구현하여 'new' 및 'popular'가 더 빠르게 로드 될 수 있도록합니다.
+# 클론 웹사이트는 order_by의 현재 선택 사항을 반영해야 합니다.
+# 메인 페이지 "/"는 기본적으로 order_by가 popular로 선택되어야 합니다.
+# 각 타이틀에 링크를 걸어 그 타이틀에 해당하는 모든 코멘트들을 볼 수 있도록 합니다.
+
+
+
+
+
+
